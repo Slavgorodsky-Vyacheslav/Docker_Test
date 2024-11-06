dd12a427be98bc93b20050bb1fa026ef8b549c4c
Slavgorodsky-Vyacheslav <41242474+Slavgorodsky-Vyacheslav@users.noreply.github.com>
2024-11-07 01:13:56 +0200

    Create terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,69 @@
+provider "aws" {
+    region = "eu-north-1"
+}
+
+terraform {
+  backend "s3" {
+    bucket = "temp-terraform-state"
+    key    = "aws/dev/terr-state/terraform.tfstate"
+    region = "eu-north-1"
+    encrypt = true
+  }
+}
+
+resource "aws_instance" "my_Ubuntu" {
+    ami = "ami-08eb150f611ca277f"
+    instance_type = "t3.micro"
+    vpc_security_group_ids = [ aws_security_group.app_sg.id ]
+    user_data = <<EOF
+#!bin/bash
+sudo apt-get -y update
+sudo apt-get -y install ca-certificates curl
+sudo install -y -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get -y update
+
+sudo apt-get -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+docker run -d -p 80:80 slavslav8/trist1
+
+docker run  \
+  --name watchtower \
+  -v /var/run/docker.sock:/var/run/docker.sock \
+  containrrr/watchtower \
+ --interval 20
+EOF
+}
+
+resource "aws_security_group" "app_sg" {
+  name        = "app_sg"
+  description = "Allow HTTP and SSH access"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1" 
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
